b96b89e6e36405732e3eb3c8d4d57a69da4a3146
Eduardo Barrio <edubp89@gmail.com>
2020-04-12 22:04:03 +0100

    Finished section 3 - Angular9 + Spring5

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,7 @@
+import { Cliente } from  './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'Edu', apellido: 'Barrio', email: 'edu@gmail.com', createAt: '2017-12-11'},
+  {id: 2, nombre: 'Ada', apellido: 'Peña', email: 'ada@gmail.com', createAt: '2017-12-10'},
+  {id: 3, nombre: 'Pedro', apellido: 'Suarez', email: 'pedro@gmail.com', createAt: '2017-12-10'}
+];
