030a2e14e7476a12b2551f198b711192ce9956a9
Eduardo Barrio <edubp89@gmail.com>
2020-04-13 19:31:11 +0100

    Added form for adding new clients

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from  './cliente';
 import { Observable, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -10,6 +10,7 @@ import { map } from 'rxjs/operators';
 })
 export class ClienteService {
   private urlEndPoint:string = 'http://localhost:8080/api/clientes';
+  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
 
   constructor(private http: HttpClient) { }
 
@@ -19,4 +20,9 @@ export class ClienteService {
       map( response => response as Cliente[])
     );
   }
+
+  create(cliente: Cliente): Observable<Cliente>{
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders});
+  }
+
 }
